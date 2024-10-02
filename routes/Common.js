6d47ee15d954e935dec94eaadcdfbbc8a7531f8f
Abhiram0304 <tanneriabhi@gmail.com>
2024-10-02 23:06:09 +0530

    Updated Common Controllers and Routes

--- routes/Common.js
+++ routes/Common.js
@@ -5,5 +5,7 @@ const {authMiddlewares} = require("../middlewares");
 const {commonController} = require('../controllers');
 
 // WRITE ROUTES HERE
+router.get("/getTopUsers",commonController.getTopUsers);
+router.get("/topEvents",commonController.topEvents);
 
 module.exports = router;
