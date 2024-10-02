de771cafc1722ad05ab65e6aa2fc19890733886d
shiavam kumar <maishivamhoo@gmail.com>
2024-10-02 15:44:26 +0530

    club backend created

--- routes/Club.js
+++ routes/Club.js
@@ -5,5 +5,19 @@ const {authMiddlewares} = require("../middlewares");
 const {clubController} = require('../controllers');
 
 // WRITE ROUTES HERE
+router.post("/clubs", authMiddlewares, clubController.createClub);
 
+router.put("/clubs/:clubId", authMiddlewares, clubController.editClub);
+
+
+router.delete("/clubs/:clubId", authMiddlewares, clubController.deleteClub);
+
+
+router.get("/clubs", clubController.getAllClubs);
+
+
+router.get("/clubs/:clubId", clubController.getClubById);
+
+
+router.get("/clubs/:clubId/events", clubController.getClubEvents);
 module.exports = router;
