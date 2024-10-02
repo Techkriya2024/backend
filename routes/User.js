0f9688eab92c045edb31110112b8b7efa31d9901
Abhiram0304 <tanneriabhi@gmail.com>
2024-10-02 23:20:24 +0530

    Updated User Routes

--- routes/User.js
+++ routes/User.js
@@ -4,10 +4,10 @@ const router = express.Router();
 const {authMiddlewares} = require("../middlewares");
 const {userController} = require("../controllers");
 
-router.put("/editUser", authMiddlewares, userController.editUser);
-router.get("/registeredEvents", authMiddlewares, userController.registeredEvents);
-router.get("/visitedEvents", authMiddlewares, userController.visitedEvents);
-router.get("/fetchRank", authMiddlewares, userController.getRank);
-router.post("/register/:eventId", authMiddlewares, userController.registerNewEvent);
+router.put("/editUser", authMiddlewares.auth, userController.editUser);
+router.get("/registeredEvents", authMiddlewares.auth, userController.registeredEvents);
+router.get("/visitedEvents", authMiddlewares.auth, userController.visitedEvents);
+router.get("/fetchRank", authMiddlewares.auth, userController.getRank);
+router.post("/register/:eventId", authMiddlewares.auth, userController.registerNewEvent);
 
 module.exports = router;
